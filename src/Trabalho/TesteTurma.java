3f5b4e67b1752f92be70fec0fdbce57baad1e1e2
Ezequiel Sousa Silva <63311919+Escowolf@users.noreply.github.com>
2021-11-07 19:57:28 -0300

    update size Turma elements

--- src/Trabalho/TesteTurma.java
+++ src/Trabalho/TesteTurma.java
@@ -5,13 +5,34 @@ public class TesteTurma {
 	public static void main(String[] args) {
 
 		Estudante e1 = new Estudante();		
-		e1.dadosEstudante("Maria", 154785, 'F');	
+		e1.dadosEstudante("Maria",154785,'F');	
 		Estudante e2 = new Estudante();		
-		e2.dadosEstudante("João", 184547, 'F');
+		e2.dadosEstudante("João",123456,'F');
+		Estudante e3 = new Estudante();		
+		e3.dadosEstudante("Laura",234237,'F');	
+		Estudante e4 = new Estudante();		
+		e4.dadosEstudante("Vasconcelos",423423,'F');
+		Estudante e5 = new Estudante();		
+		e5.dadosEstudante("Brito",213123,'F');	
+		Estudante e6 = new Estudante();		
+		e6.dadosEstudante("Jr",184547,'F');
+		Estudante e7 = new Estudante();		
+		e7.dadosEstudante("Kenga",21232,'F');	
+		Estudante e8 = new Estudante();		
+		e8.dadosEstudante("Isso",13243,'F');
 		
 		Turma turma1 = new Turma("Programacao Orientada a Objetos");
 		turma1.matricular(e1);
 		turma1.matricular(e2);
-		turma1.trancar(154785);
+		turma1.matricular(e3);
+		turma1.matricular(e4);
+		turma1.matricular(e5);
+		turma1.matricular(e6);
+		turma1.matricular(e7);
+		turma1.matricular(e8);
+		turma1.trancar(423423);
+		turma1.trancar(e2);
+		turma1.pesquisar(184547);
+		turma1.listar();
 	}
 }
--- src/Trabalho/Turma.java
+++ src/Trabalho/Turma.java
@@ -6,63 +6,60 @@ public class Turma {
 	protected int numEstudantes;
 	protected Estudante e;
 	
-	public Turma(String nomeTurma) {
+	public Turma(String nomeTurma) {//Constructor
 		this.nomeTurma = nomeTurma;
 	}
 	
-	public void matricular (Estudante e) {//– inclui estudante na turma;
+	void matricular (Estudante e) {//inclui estudante na turma;
 			estudantesVetor[numEstudantes] = e;
 			numEstudantes++; //soma 1 estudante cada vez que o método é chamado
 			System.out.println("Aluno matriculado com sucesso!");
 			System.out.println(""); //linha em branco para dar espaço
 		}
-	
-	
-	Estudante pesquisar (int matricula) {
-		for(int i=0; i<numEstudantes; i++) {
-		    if(estudantesVetor[i].matricula == matricula) { 
+		
+	Estudante pesquisar (int matricula) {//– procura no array o estudante de matrícula igual ao parâmetro passado e o retorna;
+		for(int i = 0; i<numEstudantes; i++) {
+			if(estudantesVetor[i].matricula == matricula) { 
 		    	String e = estudantesVetor[i].nome;
-		    	System.out.println("Pesquisa estudante: "+e);
-		    	break;
-		    }
-		    if (estudantesVetor[i].matricula != matricula){
-		    	System.out.println("Não existe estudante com essa matrícula");
-		    	break;
+		    	System.out.println("Pesquisa estudante: "+e+" Matrícula: "+estudantesVetor[i].matricula);
 		    }
 		}
 		return e;	
-	} //– procura no array o estudante de matrícula igual ao parâmetro passado e o retorna;
+	} 
 	
 	void trancar (int matricula) {// – exclui estudante da turma;
 		for(int i=0; i<numEstudantes; i++) {
 		    if(estudantesVetor[i].matricula == matricula) { 
+		    	System.out.println(matricula+" "+i);
 		        System.out.println("Estudante "+estudantesVetor[i].nome+" foi excluído!");
 		    	for (int j = i; j<estudantesVetor.length-1;j++) {
 		    		estudantesVetor[j] = estudantesVetor[j+1];
 		    	}
 		    	numEstudantes--;
 		    }
-		    System.out.println(numEstudantes);
-		    break;
 		}
+		System.out.println("Estudantes restantes: "+numEstudantes);
 	}
-	void listar(){
+	
+	void listar(){// - apresenta estudantes matriculados na turma
 		for (int i=0; i<numEstudantes;i++) {
 			System.out.println("Nome: "+estudantesVetor[i].nome+" Nº matrícula: "+estudantesVetor[i].matricula+" Sexo: "+estudantesVetor[i].sexo);
-			System.out.println("");
 		}
 		System.out.println(numEstudantes+" alunos matriculados em "+nomeTurma);
 		System.out.println("");
 	} //– mostra os dados de todos os estudantes da turma;
 	
 	void trancar (Estudante e) {//Sobrecarga!
-		for(int i=0; i<numEstudantes;i++) {
-		    if(estudantesVetor[i] == e) { 
-		        System.out.println("Estudante "+estudantesVetor[i].nome+" excluído com sucesso!");
-		        estudantesVetor[i]=null;
+		for(int i=0; i<numEstudantes; i++) {
+		    if(estudantesVetor[i]== e) { 
+		        System.out.println("Estudante "+estudantesVetor[i].nome+" foi excluído!");
+		    	for (int j = i; j<estudantesVetor.length-1;j++) {
+		    		estudantesVetor[j] = estudantesVetor[j+1];
+		    	}
+		    	numEstudantes--;
 		    }
-		    break;
 		}
+		System.out.println("Estudantes restantes: "+numEstudantes);
 	} 
 	
 }
